66c873c74dc8ec6901ccd23e6285ebace6eed985
MyPoZi <kxx555u.u@gmail.com>
2019-11-01 04:47:11 +0900

    update function component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import React from "react"
 
-export default class Footer extends React.Component {
-    render() {
-        return (
-            <footer>footer</footer>
-        )
-    }
-}
+const Footer = () => (
+    <footer>
+        footer
+    </footer>
+)
+
+export default Footer
