efe1ab3609997cda00f847868c39b33faccadc45
MyPoZi <kxx555u.u@gmail.com>
2019-11-01 05:01:24 +0900

    add useState使って、変更

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from '../logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import Header from "./Header"
 import Footer from "./Footer"
 
 function App() {
+  const [name, setName] = useState("Shigasy")
+  setTimeout(
+      () => { setName("banana") }, 1000
+  )
   return (
     <div className="App">
+      {name}
       <Header />
       <Footer/>
     </div>
