9aa2f58828eb4b62f9e3c6f3dac9a88094f2ab33
MyPoZi <kxx555u.u@gmail.com>
2019-11-01 04:31:29 +0900

    add footer追加

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../logo.svg';
 import './App.css';
 import Header from "./Header"
+import Footer from "./Footer"
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer/>
     </div>
   );
 }
